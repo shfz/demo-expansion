fe97166d2108f019b5353c1ae589317d7320d85d
Hi120ki <12624257+hi120ki@users.noreply.github.com>
2021-09-26 20:50:42 +0900

    add mfa script

--- csrf/fuzz/index.ts
+++ csrf/fuzz/index.ts
@@ -1,21 +1,37 @@
 import { Fuzzlib, char } from "fzlib-node";
+import * as c from "cheerio";
 
 const fl = new Fuzzlib("http://localhost");
 
 (async () => {
-  const res = await fl.http.postForm("/register", {
+  let res = await fl.http.get("/register");
+  let $ = c.load(res.data);
+  let csrf_token = $('input[name="csrf_token"]').val()
+
+  await fl.http.postForm("/register", {
     username: fl.fuzz.gen(char.lowercase()),
     password: fl.fuzz.genAscii(),
+    csrf_token: csrf_token,
   });
+
+  res = await fl.http.get("/login");
+  $ = c.load(res.data);
+  csrf_token = $('input[name="csrf_token"]').val()
 
   await fl.http.postForm("/login", {
     username: fl.fuzz.gen(char.lowercase()),
     password: fl.fuzz.genAscii(),
+    csrf_token: csrf_token,
   });
+
+  res = await fl.http.get("/");
+  $ = c.load(res.data);
+  csrf_token = $('input[name="csrf_token"]').val()
 
   await fl.http.postForm("/memo", {
     title: fl.fuzz.gen(char.lowercase()),
     text: fl.fuzz.genAscii(),
+    csrf_token: csrf_token,
   });
 
   await fl.http.get("/logout");
