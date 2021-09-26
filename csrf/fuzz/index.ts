00068f8d65dca8922384a4e096be304a86bec91d
Hi120ki <12624257+hi120ki@users.noreply.github.com>
2021-09-27 02:35:01 +0900

    add comment

--- csrf/fuzz/index.ts
+++ csrf/fuzz/index.ts
@@ -4,30 +4,36 @@ import * as c from "cheerio";
 const fl = new Fuzzlib("http://localhost");
 
 (async () => {
+  // 1度ページを表示し、CSRFトークンを取得する
   let res = await fl.http.get("/register");
   let $ = c.load(res.data);
   let csrf_token = $('input[name="csrf_token"]').val()
 
+  // CSRFトークンを使ってユーザー登録APIをテストする
   await fl.http.postForm("/register", {
     username: fl.fuzz.gen(char.lowercase()),
     password: fl.fuzz.genAscii(),
     csrf_token: csrf_token,
   });
 
+  // 1度ページを表示し、CSRFトークンを取得する
   res = await fl.http.get("/login");
   $ = c.load(res.data);
   csrf_token = $('input[name="csrf_token"]').val()
 
+  // CSRFトークンを使ってログインAPIをテストする
   await fl.http.postForm("/login", {
     username: fl.fuzz.gen(char.lowercase()),
     password: fl.fuzz.genAscii(),
     csrf_token: csrf_token,
   });
 
+  // 1度ページを表示し、CSRFトークンを取得する
   res = await fl.http.get("/");
   $ = c.load(res.data);
   csrf_token = $('input[name="csrf_token"]').val()
 
+  // CSRFトークンを使ってメモ追加APIをテストする
   await fl.http.postForm("/memo", {
     title: fl.fuzz.gen(char.lowercase()),
     text: fl.fuzz.genAscii(),
--- mfa/fuzz/index.ts
+++ mfa/fuzz/index.ts
@@ -10,17 +10,20 @@ const fl = new Fuzzlib("http://localhost");
     password: fl.fuzz.genAscii(),
   });
 
-  // ユーザー登録時に表示されるTOTPシークレットを取得
+  // ユーザー登録時に表示されるシークレットを取得
   let $ = c.load(res.data);
   let totp_secret = $('p[id="totp"]').text();
 
   await fl.http.get("/logout");
 
+  // ワンタイムパスワードを生成
+  const one_time_password = totp(totp_secret)
+
   // ワンタイムパスワードを使用してログイン
   await fl.http.postForm("/login", {
     username: fl.fuzz.gen(char.lowercase()),
     password: fl.fuzz.genAscii(),
-    totp: totp(totp_secret)
+    totp: one_time_password
   });
 
   await fl.http.postForm("/memo", {
